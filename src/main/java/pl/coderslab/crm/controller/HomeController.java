f60830569fef56e0821a2311289bcf480cabda79
BartKrawczyk <bartek.krawczyk@interia.pl>
2019-12-29 11:38:36 +0100

    First commit - project configuration

---
+++ src/main/java/pl/coderslab/crm/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.coderslab.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String homeSite() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Bart
+  Date: 29/12/2019
+  Time: 11:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Welcome to CRM - course ending project</h1>
+</body>
+</html>
